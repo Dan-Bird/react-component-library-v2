c9116948dd2a4c2ad1b036eb9b57e22d5f692dd0
Daniel <info@daniel-bird.co.uk>
2021-04-08 23:52:46 +0100

    Clear build before recompiling

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,33 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
+import clear from 'rollup-plugin-clear';
+import { terser } from 'rollup-plugin-terser';
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
     {
       file: packageJson.main,
-      format: "cjs",
+      format: 'cjs',
       sourcemap: true,
     },
     {
       file: packageJson.module,
-      format: "esm",
+      format: 'esm',
       sourcemap: true,
     },
   ],
   plugins: [
     peerDepsExternal(),
+    clear({
+      targets: ['build/'],
+      watch: true,
+    }),
     resolve(),
     postcss({ modules: true }),
     commonjs(),
