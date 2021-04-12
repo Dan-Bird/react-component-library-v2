606679196bc16f8102c370acca91b2e3e4912245
Daniel <info@daniel-bird.co.uk>
2021-04-12 09:57:11 +0100

    Update type description file

--- build/components/PrimaryButton/PrimaryButton.d.ts
+++ build/components/PrimaryButton/PrimaryButton.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="react" />
 import { BaseButtonProps } from '../BaseButton/BaseButton';
 declare const PrimaryButton: import("styled-components").StyledComponent<({ className, children, ...otherProps }: BaseButtonProps) => JSX.Element, any, BaseButtonProps, never>;
 export default PrimaryButton;
